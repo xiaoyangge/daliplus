ad251ae20bf19749bc43544d965a5583198ccb15
xiaoyangge <119925531@qq.com>
2020-04-21 23:53:33 +0800

    first commit

--- framework/api/upload_control.php
+++ framework/api/upload_control.php
@@ -95,4 +95,106 @@ class upload_control extends phpok_control
 		$rs = $this->model('res')->get_one($id);
 		$this->json($rs,true);
 	}
+
+
+
+	public function base64_f()
+	{
+		if($this->u_id){
+			if(!$this->site['upload_user']){
+				$this->json(P_Lang('你没有上传权限'));
+			}
+		}else{
+			if(!$this->site['upload_guest']){
+				$this->json(P_Lang('游客没有上传权限'));
+			}
+		}
+		$cateid = $this->get('cateid','int');
+		if($cateid){
+			$cate_rs = $this->model('rescate')->get_one($cateid);
+		}
+		if(!$cate_rs){
+			$cate_rs = $this->model('rescate')->get_default();
+			if(!$cate_rs){
+				$this->json(P_Lang('未配置附件存储方式'));
+			}
+		}
+		$filetypes = $this->u_id ? $cate_rs['filetypes'] : 'jpg,png,gif,rar,zip';
+		$this->lib('upload')->set_type($filetypes);
+		$this->lib('upload')->set_cate($cate_rs);
+
+
+			$data = $this->get('data');
+			if(!$data){
+				$this->error(P_Lang('图片内容不能为空'));
+			}
+			if(strpos($data,',') === false){
+				$this->error(P_Lang('附片格式不正确'));
+			}
+			$tmp = explode(",",$data);
+			$tmpinfo = substr($data,strlen($tmp[0]));
+			$content = base64_decode($tmpinfo);
+			if($content == $tmpinfo){
+				$this->error(P_Lang('不是合法的图片文件'));
+			}
+			$info = explode(";",$tmp[0]);
+			$ext = 'png';
+			if($info[0]){
+				$tmp = explode("/",$info[0]);
+				if($tmp[1]){
+					$ext = $tmp[1];
+				}
+			}
+			if(!in_array($ext,array('jpg','png','gif','jpeg'))){
+				$this->error(P_Lang('上传的文件格式不附合系统要求'));
+			}
+			if($ext == 'jpeg'){
+				$ext = 'jpg';
+			}
+			$file_name = $this->u_id.time().'.'.$ext;
+			$save_pic = $cate_rs['xxx'].$file_name;
+			$this->lib('file')->rm($this->dir_root.$save_pic);
+			$this->lib('file')->save_pic($content,$this->dir_root.$save_pic);
+			//生成正方式
+			//-$this->lib('gd')->thumb($this->dir_root.$save_pic,$this->u_id,100,100);
+			$this->lib('file')->mv('res/user/_'.$this->u_id.'.'.$ext,$save_pic);
+
+		$array = array();
+		$array["cate_id"] = $this->lib('upload')->get_cate();
+		$array["folder"] = $this->lib('upload')->get_folder();
+		$array["name"] = $file_name;
+		$array["ext"] = $ext;
+		$array["filename"] = $array["folder"].$file_name;
+		$array["addtime"] = $this->time;
+		$array['title'] = "";
+		//-$array["mime_type"] = $upload['mime_type'];
+		$arraylist = array("jpg","gif","png","jpeg");
+
+		if(in_array($ext,$arraylist)){
+			$img_ext = getimagesize($this->dir_root.$save_pic);
+			$my_ext = array("width"=>$img_ext[0],"height"=>$img_ext[1]);
+			$array["attr"] = serialize($my_ext);
+			$array["mime_type"] = $img_ext['mine'];
+		}
+		if(!$this->is_client){
+			$array["session_id"] = $this->session->sessid();
+		}
+		$array['user_id'] = $this->u_id;
+		$id = $this->model('res')->save($array);
+		if(!$id){
+			$this->lib('file')->rm($this->dir_root.$file_name);
+			$this->json(P_Lang('图片存储失败'));
+		}
+		$this->model('res')->gd_update($id);
+		$rs = $this->model('res')->get_one($id);
+		$this->json($rs,true);
+	}
+
+
+
+
+
+
+
+
 }
